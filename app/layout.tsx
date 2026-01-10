7e9734e16acb24ce654adbbc78b5bfae8c715d0d
ZunBee-Software <contato@zunbee.com.br>
2026-01-10 20:24:38 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 const montserrat = Montserrat({
   variable: "--font-montserrat",
