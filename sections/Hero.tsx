7a751758ba29659adfc5d8baab746852d3dadf27
ZunBee-Software <contato@zunbee.com.br>
2026-01-10 21:33:47 -0300

    Update Hero.tsx

--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -27,19 +27,6 @@ function Hero() {
       </div>
 
       <div className="flex flex-col items-center mt-10">
-        <a href='#solucoes'>
-          <button className='linearGradient rounded-xl flex items-center text-white font-semibold justify-around px-4 py-4 w-75 md:w-80'>
-            Explorar soluções ESG
-  
-            <Image
-              width={40}
-              height={40}
-              src='/icons/hero/arrow.png'
-              alt=''
-            />
-          </button>
-        </a>
-
         <a href='/dashboard'>
           <button 
             style={{color: COLORS.primary}}
